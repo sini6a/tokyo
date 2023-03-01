b1b542a5a7e488c3a1a812b9020b3f47d39cea60
sini6a <stojchevski0@gmail.com>
2023-03-01 15:44:39 +0100

    revert branch to latest

---
+++ .bashrc
@@ -0,0 +1,13 @@
+# .bashrc
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+function _update_ps1() {
+    PS1=$(powerline-shell $?)
+}
+
+if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
---
+++ dwmblocks/blocks.h
@@ -0,0 +1,23 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+	{"<\x02 ", "xbps-install -nuM | wc -l", 	120, 				0},
+
+	{"\x03﬙ ",  "awk '{ printf \"%dm\", $0/60; }' /proc/uptime", 		10, 				0},
+ 
+	{"\x04﨎 ",  "curl \"wttr.in/Gothenburg?format=%t\"", 	 1800, 				 0},
+
+	{"\x05 ",  "cat /sys/class/thermal/thermal_zone6/temp | cut -c1-2", 	1, 					0},
+
+	{"\x06 ",  "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g", 		5, 					0},
+
+	{"\x07  ",  "xkb-switch", 	1, 					10},
+
+	{"\x08⚡ ",  "if [ \"$(cat /sys/class/power_supply/BAT1/status)\" = 'Charging' ]; then echo -n '+'; else echo -n ' '; fi; cat /sys/class/power_supply/BAT1/capacity", 		5, 					0},
+
+	{"\x09 ",  "date +\"%d %b %y - %R\"", 		5, 					0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = "<";
+static unsigned int delimLen = 5;
